d6a127e0eb85cb3c6ad804df0ff72aa7d63c9b4e
lisha_crmpd <lisha_crmpd@si-tech.com.cn>
2018-05-08 21:20:02 +0800

    修改订单管理模块

--- gallery-console/src/main/java/com/kkwriter/gallery/entity/user/GlyUser.java
+++ gallery-console/src/main/java/com/kkwriter/gallery/entity/user/GlyUser.java
@@ -9,10 +9,11 @@ import com.kkwriter.gallery.entity.BaseEntity;
 
 @Entity
 public class GlyUser extends BaseEntity {
-	private static final long serialVersionUID = -2152461446126742748L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer userId;
+    private static final long serialVersionUID = -2152461446126742748L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer userId;
     private String username;
     private String phoneNum;
     private String email;
@@ -20,52 +21,68 @@ public class GlyUser extends BaseEntity {
     private Integer userLevel;
     private Integer score;
     private Integer isValid;
-	public Integer getUserId() {
-		return userId;
-	}
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPhoneNum() {
-		return phoneNum;
-	}
-	public void setPhoneNum(String phoneNum) {
-		this.phoneNum = phoneNum;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Integer getUserType() {
-		return userType;
-	}
-	public void setUserType(Integer userType) {
-		this.userType = userType;
-	}
-	public Integer getUserLevel() {
-		return userLevel;
-	}
-	public void setUserLevel(Integer userLevel) {
-		this.userLevel = userLevel;
-	}
-	public Integer getScore() {
-		return score;
-	}
-	public void setScore(Integer score) {
-		this.score = score;
-	}
-	public Integer getIsValid() {
-		return isValid;
-	}
-	public void setIsValid(Integer isValid) {
-		this.isValid = isValid;
-	}
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getUserType() {
+        return userType;
+    }
+
+    public void setUserType(Integer userType) {
+        this.userType = userType;
+    }
+
+    public Integer getUserLevel() {
+        return userLevel;
+    }
+
+    public void setUserLevel(Integer userLevel) {
+        this.userLevel = userLevel;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Integer getIsValid() {
+        return isValid;
+    }
+
+    public void setIsValid(Integer isValid) {
+        this.isValid = isValid;
+    }
 }
--- gallery-console/src/main/java/com/kkwriter/gallery/service/order/OrderServiceImpl.java
+++ gallery-console/src/main/java/com/kkwriter/gallery/service/order/OrderServiceImpl.java
@@ -1,24 +1,31 @@
 package com.kkwriter.gallery.service.order;
 
-import java.sql.Timestamp;
-import java.util.Date;
-
-import javax.annotation.Resource;
-
+import com.kkwriter.gallery.entity.order.GlyOrder;
+import com.kkwriter.gallery.exception.GalleyConsoleException;
+import com.kkwriter.gallery.repository.address.GlyAddressRepository;
+import com.kkwriter.gallery.repository.order.GlyOrderRepository;
+import com.kkwriter.gallery.repository.user.GlyUserRepository;
+import com.kkwriter.gallery.result.ReturnEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.kkwriter.gallery.entity.order.GlyOrder;
-import com.kkwriter.gallery.exception.GalleyConsoleException;
-import com.kkwriter.gallery.repository.order.GlyOrderRepository;
-import com.kkwriter.gallery.result.ReturnEnum;
+import javax.annotation.Resource;
+import java.sql.Timestamp;
 
+/**
+ * 订单服务实现类
+ * @author lisha
+ */
 @Service
 public class OrderServiceImpl implements OrderService {
 	@Resource(name = "glyOrderRepository")
 	private GlyOrderRepository glyOrderRepository;
+	@Resource(name = "glyUserRepository")
+	private GlyUserRepository glyUserRepository;
+	@Resource(name = "glyAddressRepository")
+	private GlyAddressRepository glyAddressRepository;
 
 	@Override
 	public Page<GlyOrder> getAllOrder(Pageable pageable) {
@@ -26,6 +33,10 @@ public class OrderServiceImpl implements OrderService {
 		if (!orders.hasContent()) {
 			throw new GalleyConsoleException(ReturnEnum.DO_NOT_HAVE_DATA);
 		}
+		orders.forEach((order) -> {
+			glyUserRepository.findById(order.getUserId()).ifPresent(order::setGlyUser);
+			glyAddressRepository.findById(order.getAddressId()).ifPresent(order::setGlyAddress);
+		});
 		return orders;
 	}
 
@@ -35,15 +46,22 @@ public class OrderServiceImpl implements OrderService {
 		if (!orders.hasContent()) {
 			throw new GalleyConsoleException(ReturnEnum.DO_NOT_HAVE_DATA);
 		}
+		orders.forEach((order) -> {
+			glyUserRepository.findById(order.getUserId()).ifPresent(order::setGlyUser);
+			glyAddressRepository.findById(order.getAddressId()).ifPresent(order::setGlyAddress);
+		});
 		return orders;
 	}
 
 	@Override
-	@Transactional
+	@Transactional(rollbackFor = RuntimeException.class)
 	public void editOrderPrice(String orderCode, Float price) {
-		GlyOrder order = glyOrderRepository.findById(orderCode).get();
+		GlyOrder order = glyOrderRepository.findById(orderCode).orElse(null);
+		if (order == null) {
+			throw new GalleyConsoleException(ReturnEnum.UPDATE_FAILED);
+		}
 		order.setRealPayment(price);
-		order.setUpdateTime(new Timestamp(new Date().getTime()));
+		order.setUpdateTime(new Timestamp(System.currentTimeMillis()));
 		glyOrderRepository.save(order);
 	}
 
